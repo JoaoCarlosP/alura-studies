004afcc5960fa9175a14ddb6127c0d2439b52e6e
João Carlos <joaocarlosjoe74@gmail.com>
2022-12-19 22:02:25 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import StudyList from './components/Button/StudyList/StudyList';
+import StudyForm from './components/StudyForm/StudyForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <StudyForm />
+      <StudyList />
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Button/StudyList/StudyList.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function StudyList() {
+  const tasks = [
+  {
+    task: 'React',
+    time: '02:00:00'
+  },
+  {
+    task: 'Javascript',
+    time: '01:00:00'
+  },
+  {
+    task: 'Typescript',
+    time: '03:50:24'
+  }
+]
+
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <h3>{item.task}</h3>
+            <span>{item.time}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default StudyList;
